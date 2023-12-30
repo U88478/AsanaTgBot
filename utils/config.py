0925581e832d823f9e0ce2d068d5738260f19124
U88478 <67838538+U88478@users.noreply.github.com>
2023-12-30 04:02:33 +0200

    Add files via upload

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,6 @@
+import os
+
+
 token = '6886248036:AAHb4o7QNbLQ9cP89_8s6TDHaw8M-Y85w1g'
 # pat_token = '2/1206070630058470/1206071134750827:a1bdfe76340c4edc3f12668d9bbae019'
 redirect_url = 'https://funny-certain-macaque.ngrok-free.app'
@@ -6,4 +9,7 @@ asana_client_secret = '22fc4708be564e39af7b34bf0f7bc410'
 auth_url = f'https://app.asana.com/-/oauth_authorize?client_id={asana_client_id}&response_type=code&redirect_uri={redirect_url}&scope=default'
 url = 'https://app.asana.com/'
 key = 'c8OHE_EXWAmD8yxqbMrJLrGnVfMveROTKy9gq-ocAI0='
-db_url = 'sqlite:///AsanaDB.db'
+# db_url = 'sqlite:///AsanaDB.db'
+db_url = os.getenv('DATABASE_URL')
+if db_url.startswith('postgres://'):
+    db_url = db_url.replace('postgres://', 'postgresql://')
