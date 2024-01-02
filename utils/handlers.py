b21ae862337f9aca5e641f074bc8203d16ec5e5b
U88478 <67838538+U88478@users.noreply.github.com>
2024-01-02 04:19:29 +0200

    Add files via upload

--- utils/handlers.py
+++ utils/handlers.py
@@ -8,6 +8,7 @@ from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKey
 from aiogram.filters import Command, CommandStart, StateFilter
 from aiogram.fsm.context import FSMContext
 from utils.asana_functions import *
+from db.functions import *
 from bot.bot_instance import bot
 from utils.config import *
 from utils.helpers import *
