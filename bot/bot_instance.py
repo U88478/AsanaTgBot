55e1620a67bcef1e6226aa0fc62e2cecb60edaf7
U88478 <67838538+U88478@users.noreply.github.com>
2024-01-02 04:15:22 +0200

    Add files via upload

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -1,8 +1,5 @@
+from utils.config import token
 from aiogram import Bot
 
 
-bot = None
-
-def init_bot(token: str):
-    global bot
-    bot = Bot(token=token)
+bot = Bot(token)
--- bot/main.py
+++ bot/main.py
@@ -4,13 +4,12 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.config import *
-from bot.bot_instance import init_bot, bot
+from bot.bot_instance import bot
 from utils.handlers import router
 from aiogram import Dispatcher
 
 
 async def main():
-    init_bot(token)
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
