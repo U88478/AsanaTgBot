5d26e45f511105c58ff45555be819d3048e623d5
U88478 <67838538+U88478@users.noreply.github.com>
2024-01-03 21:19:51 +0200

    Add files via upload

--- utils/handlers.py
+++ utils/handlers.py
@@ -22,8 +22,10 @@ router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message, state: FSMContext) -> None:
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Вийти"))
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton("Вийти")]],
+        resize_keyboard=True
+    )
 
     await state.set_state(Authorization.token)
     await message.reply(f"Для авторизації в Asana, будь ласка, перейдіть за наступним посиланням: \n\n{auth_url}", reply_markup=keyboard)
